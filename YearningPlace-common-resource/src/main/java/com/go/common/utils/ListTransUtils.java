d91b2ad71b26cb1fc0896986b59425db10ecc2ea
wishopen <1525306678@qq.com>
2021-03-21 16:37:39 +0800

    Utils 修改class

--- YearningPlace-common-resource/src/main/java/com/go/common/utils/ListTransUtils.java
+++ YearningPlace-common-resource/src/main/java/com/go/common/utils/ListTransUtils.java
@@ -78,18 +78,5 @@ public class ListTransUtils<T> {
 		}
 		
 	}
-	public List<T> transfor6(String json,Class<?> T){
-		try{
-			List<T> tList=null;
-			JsonNode data = MapperUtil.MP.readTree(json);
-			JsonParser parser = data.traverse();
-			CollectionType type = MapperUtil.MP.getTypeFactory().constructCollectionType(List.class, T);
-			tList=MapperUtil.MP.readValue(parser, type);
-			return tList;
-		}catch(Exception e){
-			e.printStackTrace();
-			return null;
-		}
-		
-	}
+
 }
