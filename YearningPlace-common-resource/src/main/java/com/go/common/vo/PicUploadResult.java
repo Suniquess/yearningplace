5b81f2dd591ed85a8986ede5b9a5858e2387c664
wishopen <1525306678@qq.com>
2021-03-29 22:10:34 +0800

    增加vo交互类

--- YearningPlace-common-resource/src/main/java/com/go/common/vo/PicUploadResult.java
+++ YearningPlace-common-resource/src/main/java/com/go/common/vo/PicUploadResult.java
@@ -16,5 +16,65 @@ public class PicUploadResult {
 		this.url = url;
 	}
 
-
+public static EasyUIResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("rows");
+            List<?> list = null;
+            if (data.isArray() && data.size() > 0) {
+                list = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return new EasyUIResult(jsonNode.get("total").intValue(), list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	/**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static EasyUIResult formatToList2(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("rows");
+            List<?> list = null;
+            if (data.isArray() && data.size() > 0) {
+                list = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return new EasyUIResult(jsonNode.get("total").intValue(), list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	/**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static EasyUIResult formatToList3(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("rows");
+            List<?> list = null;
+            if (data.isArray() && data.size() > 0) {
+                list = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return new EasyUIResult(jsonNode.get("total").intValue(), list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
